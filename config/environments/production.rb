f32bf1d2ff585562cc062da1f20e052545e2bd65
David Rodriguez <stoicdavid@gmail.com>
2013-10-21 00:22:16 -0500

    mailer config with gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,12 @@ Webship::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = { address: "smtp.gmail.com",
+  port: 587,
+  domain: "webship.herokuapp.com", authentication: "plain",
+  user_name: "stoicdavid@gmail.com", password: "fat719110", enable_starttls_auto: true
+  }
 end
