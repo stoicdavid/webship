23e0a09e45bf5ab6906b4e9ef611eca41a635804
David R <stoicdavid@gmail.com>
2014-02-07 04:28:47 -0600

    assets vendor

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,21 @@ module Webship
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
 
-    config.assets.paths << Rails.root.join( "vendor", "app","assets", "images", "font")
+    config.assets.precompile << Proc.new { |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
+
+        if ((full_path.starts_with? app_assets_path) || (full_path.starts_with? vendor_assets_path)) && (!path.starts_with? '_')
+          puts "\t" + full_path.slice(Rails.root.to_path.size..-1)
+          true
+        else
+          false
+        end
+      else
+        false
+      end
+    }
   end
 end
