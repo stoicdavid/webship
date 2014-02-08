f9665bb95a3adb0f5a273a8dc75c4038fba11c93
David R <stoicdavid@gmail.com>
2014-02-08 16:41:04 -0600

    workl

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140208174024) do
+ActiveRecord::Schema.define(version: 20140208222322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 20140208174024) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "description"
+    t.string   "cedis"
   end
 
   add_index "locations", ["state_id"], name: "index_locations_on_state_id", using: :btree
@@ -172,6 +173,8 @@ ActiveRecord::Schema.define(version: 20140208174024) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "authentication_token"
+    t.string   "name"
+    t.string   "last_name"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
